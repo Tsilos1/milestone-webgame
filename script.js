8a4f17e181875e298ab7900d3b12e92fdab19388
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-09-07 15:59:18 -0500

    added some pseudocode

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-//event listeners for clicks on each answer
-
-var inputs = document.getElementsByValue('earth', 'air', 'water', 'fire', 'mixed');
-
-for(var i = 0; i < inputs.length; i++){
-  inputs[i].addEventListener('click', check);
-}
-
-//check that all questions are answered
+//each question winning value
 
 
+//sum winning values (if/else if)
+//if quest1 = earth += +1 earth (else if for air, water, fire, mixed)
+//if quest2 (same as above)
+//if quest3 (same as above)
+//if quest4 (same as above)
+//if quest5 (same as above)
 
+
+
+//add totals of sum winning values for each question
+
+
+
+
+//if 2 earth, then earth wins
+//else if 2 air, then air wins
+//else if 2 water, then water wins
+//else if if 2 fire, then fire wins
+//else mixed wins
+
+
+//based on what wins, go to results page and unhide winning value
+
+
+//tally results
+// function tallyResults () {
+//     const submitButton = document.querySelector('#submitButton');        
+//     const radioButtons = document.querySelectorAll('input[name="quest1", "quest2", "quest3", "quest4", "quest5"]');
+//     submitButton.addEventListener("click", () => {
+//         let category;
+//         for (const radioButton of radioButtons) {
+//             if (radioButton.checked) {
+//                 category = radioButton.value;
+//                 break;
+//             }
+//             console.log(value)
+//         }
+//     }
+
+//Results page declare winner
+`You are the element ${value}`
