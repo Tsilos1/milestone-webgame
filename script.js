3f5c81ff467f858cb8c4a3f97ca6ca15736c227c
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-09-06 20:55:27 -0500

    updated JS

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-//submit button function
-submit.addEventListener('click', getResults);
+//event listeners for clicks on each answer
+
+var inputs = document.getElementsByValue('earth', 'air', 'water', 'fire', 'mixed');
+
+for(var i = 0; i < inputs.length; i++){
+  inputs[i].addEventListener('click', check);
+}
+
+//check that all questions are answered
 
 
 
-
-//each question must be answered alert and stop from calculating quiz
-
-
-
-
-
-//tally totals of each value
-function displayRadioValue(){
-    var score = 0;
-    var ele = document.getElementsByName('earth', 'air', 'water', 'fire', 'mixed');
-    for(i = 0; i < ele.length; i++) { 
-                if(ele[i].checked) 
-                document.getElementById("result").innerHTML
-                       score+=parseInt(ele[i].value)
-    }
-    }
-
-// $("input[type=radio]").click(function() {
-//     var total = 0;
-//     $("input[type=radio]:checked").each(function() {
-//         total += parseFloat($(this).val());
-//     });
- 
-//     $(".totalSum").val(total);
-// });
-
-
-
-
-//compare totals of each value
-
-
-
-
-
-//Display results page
-//hide results that do not apply
